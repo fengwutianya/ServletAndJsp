736e0a731115383904a482a6d1e67fea1d384274
fengwutianya <949979029@qq.com>
2016-10-23 16:01:32 +0800

    init

---
+++ src/servlet/MyFirstServlet.java
@@ -0,0 +1,16 @@
+package servlet;
+
+import java.io.IOException;
+
+/**
+ * Created by xuan on 2016/10/23 0023.
+ */
+public class MyFirstServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
